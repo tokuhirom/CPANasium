b06dc626e55435cad15390499735092d79d78bd7
tokuhirom <tokuhirom@gmail.com>
2013-05-24 14:29:51 +0900

    sql/mysql.sql

---
+++ sql/mysql.sql
@@ -0,0 +1,34 @@
+CREATE TABLE repos (
+    id integer not null primary key auto_increment,
+    name varchar(255),
+    owner_login varchar(255),
+    full_name varchar(255),
+    master_branch varchar(255),
+    html_url text,
+    owner_avatar_url varchar(255),
+    description varchar(255),
+    watchers integer,
+    forks    integer,
+    data      text,
+    cpanfile  text,
+    updated_at integer,
+    created_at integer,
+    index (owner_login),
+    unique (full_name),
+    index (updated_at)
+) ENGINE=InnoDB charset=utf8;
+
+create table deps (
+    repos_full_name varchar(255) not null,
+    -- configure,develop,test,runtime,build
+    phase varchar(255) not null,
+    -- requires, recommends, suggests
+    relationship varchar(255) not null,
+    module varchar(255) not null,
+    version varchar(255) not null,
+    created_on integer,
+    index (module),
+    index (repos_full_name),
+    unique (repos_full_name, phase, relationship, module)
+) ENGINE=InnoDB charset=utf8;
+
